77d6ce354aed9f7733fefbb07e72011a99df03b9
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-06-16 14:41:53 +0700

    Refactor the Apollo + Gatsby hack component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,16 +8,4 @@
 
 import "./src/styles/index.css"
 
-import ApolloClient from "apollo-boost"
-import React from "react"
-import fetch from "isomorphic-fetch"
-import { ApolloProvider } from "@apollo/react-hooks"
-
-const client = new ApolloClient({
-  uri: "/.netlify/functions/graphql",
-  fetch,
-})
-
-export const wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>{element}</ApolloProvider>
-)
+export { wrapRootElement } from "./src/misc/apollo-wrapped-root-element"
