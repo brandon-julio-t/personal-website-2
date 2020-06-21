2c9a3a0f9f080b60023e1cb3850ccc219eef18a8
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-06-21 11:43:34 +0700

    Eager load certificates component

--- src/components/pages/index/certificates.tsx
+++ src/components/pages/index/certificates.tsx
@@ -1,8 +1,19 @@
 import React from "react"
-import loadable from "@loadable/component"
 
 import Button from "../../common/button"
+
+import APIsAndMicroservices from "../../images/certificates/apis-and-microservices"
+import CyberSecurityIsACareerOrProfessionalServices from "../../images/certificates/cyber-security-is-a-career-or-professional-services"
+import FrontEndLibraries from "../../images/certificates/front-end-libraries"
 import IndexSection from "./components/index-section"
+import JavaBasic from "../../images/certificates/java-basic"
+import JavaScriptAlgorithmsAndDataStructures from "../../images/certificates/javascript-algorithms-and-data-structures"
+import JavaScriptBasic from "../../images/certificates/javascript-basic"
+import ReactBasic from "../../images/certificates/react-basic"
+import ResponsiveWebDesign from "../../images/certificates/responsive-web-design"
+import ScrumFoundationProfessionalCertificate from "../../images/certificates/scrum-foundation-professional-certificate"
+import TokopediaStartSummit from "../../images/certificates/tokopedia-start-summit"
+import TypingSpeed from "../../images/certificates/typing-speed"
 
 export default () => (
   <IndexSection title="Certificates">
@@ -18,79 +29,57 @@ export default () => (
 
 const certificates = [
   {
-    Component: loadable(() =>
-      import(`../../images/certificates/responsive-web-design`)
-    ),
+    Component: ResponsiveWebDesign,
     label: "Responsive Web Design Certificate",
     url: "https://bit.ly/35OoTpT",
   },
   {
-    Component: loadable(() =>
-      import(
-        `../../images/certificates/javascript-algorithms-and-data-structures`
-      )
-    ),
+    Component: JavaScriptAlgorithmsAndDataStructures,
     label: "JavaScript Algorithms and Data Structures Certificate",
     url: "https://bit.ly/3dA7eFe",
   },
   {
-    Component: loadable(() =>
-      import(`../../images/certificates/front-end-libraries`)
-    ),
+    Component: FrontEndLibraries,
     label: "Front End Libraries Certificate",
     url: "https://bit.ly/2YTCNpg",
   },
   {
-    Component: loadable(() =>
-      import(`../../images/certificates/apis-and-microservices`)
-    ),
+    Component: APIsAndMicroservices,
     label: "APIs and Microservices Certificate",
     url: "https://bit.ly/301Cvx8",
   },
   {
-    Component: loadable(() => import(`../../images/certificates/java-basic`)),
+    Component: JavaBasic,
     label: "Java Basic Certificate by HackerRank",
     url: "https://bit.ly/2YjFn5V",
   },
   {
-    Component: loadable(() =>
-      import(`../../images/certificates/javascript-basic`)
-    ),
+    Component: JavaScriptBasic,
     label: "JavaScript Basic Certificate by HackerRank",
     url: "https://bit.ly/2UpwkQ0",
   },
   {
-    Component: loadable(() => import(`../../images/certificates/react-basic`)),
+    Component: ReactBasic,
     label: "React Basic Certificate by HackerRank",
     url: "https://bit.ly/2MK8kmd",
   },
   {
-    Component: loadable(() =>
-      import(`../../images/certificates/tokopedia-start-summit`)
-    ),
+    Component: TokopediaStartSummit,
     label: "Tokopedia START Summit Certificate",
     url: "https://bit.ly/2xPxg83",
   },
   {
-    Component: loadable(() =>
-      import(
-        `../../images/certificates/cyber-security-is-a-career-or-professional-services`
-      )
-    ),
+    Component: CyberSecurityIsACareerOrProfessionalServices,
     label:
       "Cyber Security is a Career or Professional Services Seminar Participate Certificate",
     url: "https://bit.ly/2XX75H1",
   },
   {
-    Component: loadable(() =>
-      import(
-        `../../images/certificates/scrum-foundation-professional-certificate`
-      )
-    ),
+    Component: ScrumFoundationProfessionalCertificate,
     label: "Scrum Foundation Professional Certificate",
   },
   {
-    Component: loadable(() => import(`../../images/certificates/typing-speed`)),
+    Component: TypingSpeed,
     label: "Typing Speed Certificate by RataType",
     url: "https://bit.ly/2WH3M4U",
   },
