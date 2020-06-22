f651b7dd96fe3fed33c18c445c4ace2bc40dafe0
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-06-22 10:00:18 +0700

    Scroll CV to view immediately

--- src/pages/curriculum-vitae.tsx
+++ src/pages/curriculum-vitae.tsx
@@ -1,15 +1,21 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import Layout from "../layouts"
 
-export default () => (
-  <Layout title="Curriculum Vitae">
-    <iframe
-      frameBorder="0"
-      scrolling="no"
-      src="https://onedrive.live.com/embed?cid=D5A3B6C8950A0143&resid=D5A3B6C8950A0143%219596&authkey=AMRg4MEOP4uG04M&em=2"
-      title="curriculum-vitae"
-      className="bg-gray-300 rounded w-full h-screen hover:shadow transition duration-300"
-    />
-  </Layout>
-)
+export default () => {
+  useEffect(() => {
+    document.querySelector("iframe").scrollIntoView({ behavior: "smooth" })
+  }, [])
+
+  return (
+    <Layout title="Curriculum Vitae">
+      <iframe
+        frameBorder="0"
+        scrolling="no"
+        src="https://onedrive.live.com/embed?cid=D5A3B6C8950A0143&resid=D5A3B6C8950A0143%219596&authkey=AMRg4MEOP4uG04M&em=2"
+        title="curriculum-vitae"
+        className="bg-gray-300 rounded w-full h-screen hover:shadow transition duration-300"
+      />
+    </Layout>
+  )
+}
